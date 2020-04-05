aedb4a8ce655b3e5421a353894f8b99ab016ac07
IT <itdev1210@gmail.com>
2020-04-06 04:51:13 +0800

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Responsive Chat-app Static pages
+
+## Introduction
+Responsive static pages for a dating website.
+
+## Screenshot
+
+![Image1](Screenshot_1.png)
+![Image1](Screenshot_2.png)
+![Image1](Screenshot_3.png)
+![Image1](Screenshot_4.png)
+![Image1](Screenshot_5.png)
+![Image1](Screenshot_6.png)
+
---
+++ front_messages/action.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    // function handleHeaderAdmin() {
+    //     document.getElementById("headerAdminDropdown").classList.toggle("show");
+    // }
+    // function handleRightNav() {
+    //     document.getElementById("rightNavDropdown").classList.toggle("show");
+    // }
+    // function handleUserList() {
+    //     document.getElementById("userListDropdown").classList.toggle("show");
+    // }
+    // function handleUserProfile() {
+    //     document.getElementById("userProfileDropdown").classList.toggle("show");
+    // }
+
+    // // Close the dropdown if the user clicks outside of it
+    // window.onclick = function (event) {
+    //     if (!event.target.matches('.header-admin-dropbtn')) {
+    //         var dropdowns = document.getElementsByClassName("header-admin-dropdown-content");
+    //         var i;
+    //         for (i = 0; i < dropdowns.length; i++) {
+    //             var openDropdown = dropdowns[i];
+    //             if (openDropdown.classList.contains('show')) {
+    //                 openDropdown.classList.remove('show');
+    //             }
+    //         }
+    //     }
+    //     if (!event.target.matches('.right-nav-dropbtn')) {
+    //         var dropdowns = document.getElementsByClassName("right-nav-content");
+    //         var i;
+    //         for (i = 0; i < dropdowns.length; i++) {
+    //             var openDropdown = dropdowns[i];
+    //             if (openDropdown.classList.contains('show')) {
+    //                 openDropdown.classList.remove('show');
+    //             }
+    //         }
+    //     }
+    //     if (!event.target.matches('.header-user-list-dropbtn')) {
+    //         var dropdowns = document.getElementsByClassName("user-list-content");
+    //         var i;
+    //         for (i = 0; i < dropdowns.length; i++) {
+    //             var openDropdown = dropdowns[i];
+    //             if (openDropdown.classList.contains('show')) {
+    //                 openDropdown.classList.remove('show');
+    //             }
+    //         }
+    //     }
+    //     if (!event.target.matches('.user-profile-dropbtn')) {
+    //         var dropdowns = document.getElementsByClassName("user-profile-content");
+    //         var i;
+    //         for (i = 0; i < dropdowns.length; i++) {
+    //             var openDropdown = dropdowns[i];
+    //             if (openDropdown.classList.contains('show')) {
+    //                 openDropdown.classList.remove('show');
+    //             }
+    //         }
+    //     }
+    // }
+  
+}); 
